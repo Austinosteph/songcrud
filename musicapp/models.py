4287f1c3e93449df11fc780de04ad6cceae2ab76
Austinosteph <Austinosteph@gmail.com>
2022-10-16 20:58:39 +0100

    migration

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+class song(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateField(default=datetime.today)
+    released = models.DateField()
+    likes = models.BooleanField(default=True)
+    artiste_id = models.CharField(max_length=15)
+
+class lyric(models.Model):
+    content = models.CharField(max_length=100)
+    song_id = models.CharField(max_length=15)
