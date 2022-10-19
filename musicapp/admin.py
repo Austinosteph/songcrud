dd6a6cb4bb80491185622c66779b68c2c06eec5f
Austinosteph <Austinosteph@gmail.com>
2022-10-19 21:57:33 +0100

    update

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import artiste, song, lyric
-# Register your models here.
 
 admin.site.register(artiste)
 admin.site.register(song)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from datetime import datetime
 
-
 # Create your models here.
+
 class artiste(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -11,20 +11,24 @@ class artiste(models.Model):
     def __str__(self):
         return self.first_name
     
-
 class song(models.Model):
     artiste_id = models.ForeignKey(artiste, on_delete = models.CASCADE)
     title = models.CharField(max_length=50)
     date = models.DateField(default=datetime.today)
     released = models.DateField(default=datetime.today)
     likes = models.BooleanField(default=True)
+    artiste_id = models.IntegerField()
     
     def __str__(self):
         return self.title
+    
 
 class lyric(models.Model):
-    song_id = models.ForeignKey(song, on_delete = models.CASCADE)
-    content = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.song_id
+     song_id = models.ForeignKey(song, on_delete = models.CASCADE)
+     content = models.CharField(max_length=100)
+     song_id = models.IntegerField()
+     
+     def __str__(self):
+         return self.song_id
+     
+        
